2dbd1cd85704abc9d1efcdf1f32c0e876c72fdd4
paulapompeo <paulapompeoc@gmail.com>
2021-04-03 20:21:50 +0200

    cria reducer

--- redux/src/store/index.ts
+++ redux/src/store/index.ts
@@ -1,6 +1,7 @@
 // toda configuracao do redux da aplicaçao (é como se fosse um estado global)
 import { createStore } from 'redux';
+import rootReducer from './modules/rootReducer';
 
-const store = createStore(() => [])
+const store = createStore(rootReducer)
 
 export default store;
